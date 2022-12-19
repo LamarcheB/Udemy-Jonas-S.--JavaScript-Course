0aeac2110c542d7e82e4595f617f6e55b8f91587
BenLamarche <Lamarche.w.benjamin@gmail.com>
2022-12-18 23:10:35 -0500

    Learning about JS

--- script.js
+++ script.js
@@ -7,4 +7,25 @@ let variable = false;
 console.log(typeof variable);
 */
 
-console.log(100 + 10 + 15);
+// console.log(100 + 10 + 15);
+
+// console.log("23" - 10 + 5 + "6");
+
+// const favorite = prompt("what's your favorite number?");
+// console.log(favorite);
+
+function youAreAmazing() {
+  console.log("you're amazing!");
+}
+
+youAreAmazing();
+
+function juiceMaker(apple, orange) {
+  const juice = `Juice with ${apple} apples and ${orange} oranges`;
+  return juice;
+}
+
+console.log(juiceMaker(3, 6));
+
+const appleJuice = juiceMaker(5, 2);
+console.log(appleJuice);
